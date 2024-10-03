9d78016620c4650749f6dfc1bf74293cda72c978
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-10-03 11:08:11 +0200

    nettoyage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,10 @@
-import { Outlet } from "react-router-dom";
-
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
-
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Nav />
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main>
+      <h1>portfolio</h1>
+    </main>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
-import Home from "./pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "", element: <Home /> }],
   },
 ]);
 
