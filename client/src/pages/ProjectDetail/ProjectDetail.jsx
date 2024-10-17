bd9fef17b7d79b6be6a7a7d09ee2a482f82da23c
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-10-17 13:21:50 +0200

    mise en place de modales pour ouvrir les images d'exemples en grand et modifications de style

--- client/src/pages/ProjectDetail/ProjectDetail.jsx
+++ client/src/pages/ProjectDetail/ProjectDetail.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData } from "react-router-dom";
+import Modal from "react-modal";
+import { useState } from "react";
 
 import github from "../../assets/images/icons/github.svg";
 import users from "../../assets/images/icons/users.svg";
@@ -9,18 +11,30 @@ import "./ProjectDetail.css";
 function ProjectDetail() {
   const project = useLoaderData();
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [activeImage, setActiveImage] = useState(null);
+  const openModal = (image) => {
+    setActiveImage(image);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setActiveImage(null);
+  };
+
   return (
     <>
       <h2>{project.name}</h2>
       <section className="page-display">
         <section className="project-demo">
           <img
-            className="primary-image"
+            className="primary-image box"
             src={project.img1}
             alt="exemple du site 1"
           />
           <div className="technics">
-            <article className="badges">
+            <article className="badges box">
               <img src={users} alt="icone groupe" />
               <p className="badge">{project.category}</p>
               <img src={cpu} alt="icone cpu" />
@@ -29,7 +43,8 @@ function ProjectDetail() {
               <a
                 className="badge badge-link"
                 href={project.github_link}
-                target="blank"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Github
               </a>
@@ -37,12 +52,13 @@ function ProjectDetail() {
               <a
                 className="badge badge-link"
                 href={project.website_link}
-                target="blank"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Application déployée
               </a>
             </article>
-            <div className="project-team-skills">
+            <div className="project-team-skills box">
               <article className="team">
                 <h3>L'équipe :</h3>
                 <p>{project.team}</p>
@@ -54,7 +70,7 @@ function ProjectDetail() {
             </div>
           </div>
         </section>
-        <section className="project-description-organisation">
+        <section className="project-description-organisation box">
           <article className="project-description">
             <h3>L'application :</h3>
             <p>{project.description}</p>
@@ -64,12 +80,67 @@ function ProjectDetail() {
             <p>{project.organization}</p>
           </article>
           <article className="project-images">
-            <img src={project.img2} alt="exemple du site 2" />
-            <img src={project.img3} alt="exemple du site 3" />
-            <img src={project.img4} alt="exemple du site 4" />
+            <button
+              type="button"
+              onClick={() => openModal(project.img2)}
+              tabIndex="0"
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  openModal(project.img2);
+                }
+              }}
+            >
+              <img src={project.img2} alt="exemple du site 2" />
+            </button>
+            <button
+              type="button"
+              onClick={() => openModal(project.img3)}
+              tabIndex="0"
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  openModal(project.img3);
+                }
+              }}
+            >
+              <img src={project.img3} alt="exemple du site 3" />
+            </button>
+            <button
+              type="button"
+              onClick={() => openModal(project.img4)}
+              tabIndex="0"
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  openModal(project.img4);
+                }
+              }}
+            >
+              <img src={project.img4} alt="exemple du site 4" />
+            </button>{" "}
           </article>
         </section>
       </section>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        className="Modal"
+      >
+        {activeImage && (
+          <img
+            className="image-modal"
+            src={activeImage}
+            alt="exemple du site"
+          />
+        )}
+        <button
+          type="button"
+          className="button-close-modal"
+          onClick={closeModal}
+        >
+          X
+        </button>
+      </Modal>
     </>
   );
 }
