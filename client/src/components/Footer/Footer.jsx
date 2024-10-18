c32f379334bc173ce4aba678d4fa799f1651c337
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-10-18 12:02:15 +0200

    mise enplace du front pour récupérer les citations du back

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import connexion from "../../services/connexion";
 import "./Footer.css";
 
 function Footer() {
+  const [quote, setQuote] = useState("");
+
+  const fetchQuote = async () => {
+    try {
+      const response = await connexion.get("/api/quote");
+      setQuote(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <footer>
       <section>
@@ -8,6 +25,7 @@ function Footer() {
         <p>Lubin Chauvreau</p>
         <p>06 . 72 . 14 . 43 . 08</p>
         <p>lubin-chauvreau@laposte.net</p>
+        <p>{quote}</p>
       </section>
     </footer>
   );
