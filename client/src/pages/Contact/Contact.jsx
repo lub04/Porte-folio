289595f0551b1897ac3d34285be7c4dd06ffa08f
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-12-08 17:23:55 +0100

    creation du formulaire de conatct en front

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -4,6 +4,24 @@ function Contact() {
   return (
     <div className="page-display">
       <h2>Contactez moi !</h2>
+      <form className="contact-form box">
+        <label className="user-name">
+          Nom :
+          <input type="text" />
+        </label>
+        <label className="user-name">
+          Prénom :
+          <input type="text" />
+        </label>
+        <label className="user-mail">
+          Email :
+          <input type="mail" />
+        </label>
+        <label className="user-message">
+          Message :
+          <textarea className="message" />
+        </label>
+      </form>
     </div>
   );
 }
